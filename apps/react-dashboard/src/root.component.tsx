5ad29976bd2dd7902ef4b89ca369ed6463dd6915
Grisha <gvasilyan97@gmail.com>
2022-07-14 13:56:01 +0400

    add production urls

--- apps/react-dashboard/src/root.component.tsx
+++ apps/react-dashboard/src/root.component.tsx
@@ -1,3 +1,3 @@
 export default function Root(props) {
-  return <section>React Dashboard Running on the port 9003</section>;
+  return <section>React Dashboard Running on the port 9003 Testttttttttt</section>;
 }
